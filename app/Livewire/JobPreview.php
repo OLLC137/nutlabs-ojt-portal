d4bd267fe996d2715aa947a7df364d0004baf30e
JaharaKateB <21-01880@g.batstate-u.edu.ph>
2024-10-10 10:45:18 +0800

    Removed ojt contact people

--- app/Livewire/JobPreview.php
+++ app/Livewire/JobPreview.php
@@ -12,7 +12,7 @@ class JobPreview extends Component
     public $jobInfo;
     #[Url] public $id = 1;
 
-    
+
     public function render()
     {
         $this->updateJobInfo($this->id);
@@ -24,19 +24,14 @@ class JobPreview extends Component
         $this->jobInfo = DB::table('ojt_job_listings')
         ->join('ojt_companies', 'ojt_job_listings.company_id', '=', 'ojt_companies.id')
         ->join('ojt_job_list_categories', 'ojt_job_listings.job_category', '=', 'ojt_job_list_categories.id')
-        ->join('ojt_contact_people', 'ojt_job_listings.job_person','=','ojt_contact_people.id')
         ->where('ojt_job_listings.id', $jobId)
         ->select(
-            'ojt_job_listings.id as job_id', 
-            'ojt_job_listings.job_list as job_list', 
+            'ojt_job_listings.id as job_id',
+            'ojt_job_listings.job_list as job_list',
             'ojt_job_list_categories.id as job_category_id', // Include category ID
             'ojt_job_list_categories.cat_name as job_category', // Select cat_name instead of job_category
             'ojt_job_listings.job_desc as job_desc',
             'ojt_job_listings.job_ref as job_ref',
-            'ojt_contact_people.contact_name as job_person',
-            'ojt_contact_people.contact_position as position',
-            'ojt_contact_people.contact_email as job_email',
-            'ojt_contact_people.contact_contact as job_contact',
             'ojt_companies.co_name as company_name',
             'ojt_companies.co_address as location'
         )
@@ -49,16 +44,16 @@ class JobPreview extends Component
             'blue', 'orange', 'yellow', 'green', 'red', 'purple', 'navy', 'green', 'gray', 'silver',
             'brown', 'black', 'pink', 'skyblue', 'gold', 'purple', 'green', 'gray', 'lightblue', 'green'
         ];
-    
+
         // Calculate the index based on categoryId
         $index = ($categoryId - 1) % count($colors);
-    
+
         // Build the style string with background-color and optional color
         $style = 'background-color: ' . $colors[$index] . ';';
         if (in_array($colors[$index], ['navy', 'black', 'purple', 'brown', 'blue','red','green'])) {
             $style .= ' color: white;'; // Add white text for dark backgrounds
         }
-    
+
         return $style;
     }
 }
