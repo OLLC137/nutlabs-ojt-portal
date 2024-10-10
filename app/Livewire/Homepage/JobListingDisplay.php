dba8d240adc185f88b4749d738a066855cc78fb4
OLLC137 <jaunaromin@gmail.com>
2024-10-10 10:45:22 +0800

    Removed landing page contact

--- app/Livewire/Homepage/JobListingDisplay.php
+++ app/Livewire/Homepage/JobListingDisplay.php
@@ -15,10 +15,9 @@ class JobListingDisplay extends Component
         $this->jobListings = DB::table('ojt_job_listings')
             ->join('ojt_companies', 'ojt_job_listings.company_id', '=', 'ojt_companies.id')
             ->join('ojt_job_list_categories', 'ojt_job_listings.job_category', '=', 'ojt_job_list_categories.id')
-            ->join('ojt_contact_people', 'ojt_job_listings.job_person','=','ojt_contact_people.id')
             ->select(
-                'ojt_job_listings.id as job_id', 
-                'ojt_job_listings.job_list as job_list', 
+                'ojt_job_listings.id as job_id',
+                'ojt_job_listings.job_list as job_list',
                 'ojt_job_list_categories.id as job_category_id', // Include category ID
                 'ojt_job_list_categories.cat_name as job_category', // Select cat_name instead of job_category
                 'ojt_job_listings.job_desc as job_desc',
@@ -56,16 +55,16 @@ class JobListingDisplay extends Component
             'blue', 'orange', 'yellow', 'green', 'red', 'purple', 'navy', 'green', 'gray', 'silver',
             'brown', 'black', 'pink', 'skyblue', 'gold', 'purple', 'green', 'gray', 'lightblue', 'green'
         ];
-    
+
         // Calculate the index based on categoryId
         $index = ($categoryId - 1) % count($colors);
-    
+
         // Build the style string with background-color and optional color
         $style = 'background-color: ' . $colors[$index] . ';';
         if (in_array($colors[$index], ['navy', 'black', 'purple', 'brown', 'blue','red','green'])) {
             $style .= ' color: white;'; // Add white text for dark backgrounds
         }
-    
+
         return $style;
     }
 }
--- app/Livewire/JobListPage.php
+++ app/Livewire/JobListPage.php
@@ -2,7 +2,7 @@
 
 namespace App\Livewire;
 
-use Livewire\Attributes\On; 
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
@@ -11,7 +11,7 @@ use Livewire\WithPagination;
 class JobListPage extends Component
 {
     use WithPagination;
-    
+
     #[Url] public $id;
     #[Url] public $search;
     #[Url] public $category;
@@ -21,7 +21,7 @@ class JobListPage extends Component
 
     public function render()
     {
-        
+
         // Initial query without search filters
         $query = DB::table('ojt_job_listings')
         ->join('ojt_companies', 'ojt_job_listings.company_id', '=', 'ojt_companies.id')
@@ -64,11 +64,11 @@ class JobListPage extends Component
             $jobExists = DB::table('ojt_job_listings')
                 ->where('id', $this->id)
                 ->exists();
-    
+
             if (!$jobExists) {
                 abort(404); // Job not found, return 404 response
             }
-    
+
             $this->updateJobInfo($this->id);
         }
 
@@ -89,7 +89,7 @@ class JobListPage extends Component
         $this->category = $category;
         $this->location = $location;
         $this->id = null;
-        
+
         $this->resetPage(); // Reset pagination to show results from page 1
 
     }
@@ -98,19 +98,14 @@ class JobListPage extends Component
         $this->jobInfo = DB::table('ojt_job_listings')
         ->join('ojt_companies', 'ojt_job_listings.company_id', '=', 'ojt_companies.id')
         ->join('ojt_job_list_categories', 'ojt_job_listings.job_category', '=', 'ojt_job_list_categories.id')
-        ->join('ojt_contact_people', 'ojt_job_listings.job_person','=','ojt_contact_people.id')
         ->where('ojt_job_listings.id', $jobId)
         ->select(
-            'ojt_job_listings.id as job_id', 
-            'ojt_job_listings.job_list as job_list', 
+            'ojt_job_listings.id as job_id',
+            'ojt_job_listings.job_list as job_list',
             'ojt_job_list_categories.id as job_category_id', // Include category ID
             'ojt_job_list_categories.cat_name as job_category', // Select cat_name instead of job_category
             'ojt_job_listings.job_desc as job_desc',
             'ojt_job_listings.job_ref as job_ref',
-            'ojt_contact_people.contact_name as job_person',
-            'ojt_contact_people.contact_position as position',
-            'ojt_contact_people.contact_email as job_email',
-            'ojt_contact_people.contact_contact as job_contact',
             'ojt_companies.co_name as company_name',
             'ojt_companies.co_address as location'
         )
@@ -123,16 +118,16 @@ class JobListPage extends Component
             'blue', 'orange', 'yellow', 'green', 'red', 'purple', 'navy', 'green', 'gray', 'silver',
             'brown', 'black', 'pink', 'skyblue', 'gold', 'purple', 'green', 'gray', 'lightblue', 'green'
         ];
-    
+
         // Calculate the index based on categoryId
         $index = ($categoryId - 1) % count($colors);
-    
+
         // Build the style string with background-color and optional color
         $style = 'background-color: ' . $colors[$index] . ';';
         if (in_array($colors[$index], ['navy', 'black', 'purple', 'brown', 'blue','red','green'])) {
             $style .= ' color: white;'; // Add white text for dark backgrounds
         }
-    
+
         return $style;
     }
 }
