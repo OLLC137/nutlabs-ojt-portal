b28eeeefd9e21d4daebb3c19da652a25e25e331a
OLLC137 <jaunaromin@gmail.com>
2024-10-30 23:57:43 +0800

    Fixed Middleware for student posting

--- app/Http/Middleware/CheckStudentReqs.php
+++ app/Http/Middleware/CheckStudentReqs.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use App\Models\OjtRequirement;
+use App\Models\OjtStudent;
 
 class CheckStudentReqs
 {
@@ -20,8 +21,11 @@ class CheckStudentReqs
         // Get the current authenticated user
         $user = Auth::user();
 
+        // Get the user's student equivalent
+        $student = OjtStudent::where('user_id', $user->id)->first();
+
         // Fetch the user's req_ids from the ojt_requirements table
-        $userReqIds = OjtRequirement::where('student_id', $user->id)
+        $userReqIds = OjtRequirement::where('student_id', $student->id)
             ->whereNotNull('locked_at')
             ->pluck('req_id')
             ->toArray();
@@ -32,10 +36,10 @@ class CheckStudentReqs
         // Check if user has all required req_ids
         $hasAllRequirements = count(array_intersect($requiredReqIds, $userReqIds)) === count($requiredReqIds);
 
+        // Redirect to the requirements page if the user does not have all required req_ids
         if (!$hasAllRequirements) {
             return redirect()->route('intern-requirements')->with('error', 'You cannot access the accomplishment page until all pre-ojt requirements are submitted.');
         }
-
 
         return $next($request);
     }
