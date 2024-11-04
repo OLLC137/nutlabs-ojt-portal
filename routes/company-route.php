c894d38023616b209800696d433d6c4560793376
JaharaKateB <21-01880@g.batstate-u.edu.ph>
2024-11-04 20:55:28 +0800

    Removed OJT Coordinator's access to edit company information

--- routes/company-route.php
+++ routes/company-route.php
@@ -6,6 +6,7 @@ Route::middleware([
     'auth',
     'role:4,0'
 ])->group(function () {
+    
     Route::get('/view-applicants', fn() => view('pages.view-applicants'))->name('view-applicants');
     Route::get('/edit', fn() => view('pages.company-edit-info-page'))->name('edit');
 });
