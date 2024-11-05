4d60c54e4a67df597b3ba626ff4d0db3c9e8f188
hazzeeella <haaagapito@gmail.com>
2024-11-05 20:48:30 +0800

    Header Company Dashboard Route

--- routes/company-route.php
+++ routes/company-route.php
@@ -9,5 +9,6 @@ Route::middleware([
 ])->group(function () {
     Route::get('/view-applicants', fn() => view('pages.view-applicants'))->name('view-applicants');
     Route::get('/edit', fn() => view('pages.company-edit-info-page'))->name('edit');
-    Route::get('/applicant-dashboard', fn() => view('livewire.applicant-dashboard'))->name('applicant-dashboard');
+    Route::get('/applicant-dashboard', fn() => view('pages.view-applicant-dashboard'))->name('applicant-dashboard');
 });
+
