4c4338bce07dc6318a2d109a41d983f8123b0638
hazzeeella <haaagapito@gmail.com>
2024-11-27 22:41:03 +0800

    edited

--- resources/views/livewire/manage-job-list.blade.php
+++ resources/views/livewire/manage-job-list.blade.php
@@ -19,27 +19,13 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col-xl-6 p-4 border">
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <h1>{{ $jobInfo->job_list }}</h1>
-                                    <h4 class="bg-primary text-white p-1 rounded">{{ $jobInfo->job_category }}</h4>
-                                </div>
-                                <p class="display-5 font-weight-bold mb-0">{{ $jobInfo->company_name }}</p>
-                                <p class="">
-                                    <x-template.icon>map-marker-outline</x-template.icon>
-                                    {{ $jobInfo->location }}
-                                </p>
-                                <div class="mb-4">
-                                    {!! $jobInfo->job_desc !!}
-                                </div>
+
+
+
                                 <p class="font-weight-bold">Recommended Programs</p>
                                 <div class="d-flex flex-row text-white flex-wrap">
-                                    @foreach ($jobPrograms as $program)
-                                        <p class="bg-secondary p-1 mx-1 rounded">
-                                            <x-template.icon>tag-outline</x-template.icon>
-                                            {{ $program }}
-                                        </p>
-                                    @endforeach
-                                </div>
+
+
                                 <div>
                                     <p class="font-weight-bold">Provided Files</p>
                                     <div class="d-block">
