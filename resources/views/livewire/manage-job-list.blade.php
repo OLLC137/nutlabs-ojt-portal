ad8f3ec6eec35dc6020835a24acd8cf40a56cc9e
hazzeeella <haaagapito@gmail.com>
2024-11-28 01:31:42 +0800

    edit name, info, location, desc, category

--- resources/views/livewire/manage-job-list.blade.php
+++ resources/views/livewire/manage-job-list.blade.php
@@ -9,100 +9,96 @@
         <x-template.icon class="btn-icon-prepend">subdirectory-arrow-left</x-template.icon>
     back
     </button>
+
     <div class="card my-4 py-2 px-2">
         <div class="card-body">
-            <div class="card-title">
-                <h2>Add a new job list</h2>
-            </div>
             <div class="row">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-xl-6 p-4 border">
+                <div class="col-xl-6 p-4 border">
+                    <h1>{{ $jobInfo->job_list }}</h1>
+                    <div class="form-group mb-0">
+                        <h4 class="bg-primary text-white p-1 rounded">{{ $selectedCategoryName }}</h4>
 
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <h1>{{ $jobInfo->job_list }}</h1>
-                                    <h4 class="bg-primary text-white p-1 rounded">{{ $selectedCategoryName }}</h4>
-                                </div>
-                                <p class="display-5 font-weight-bold mb-0">{{ $selectedCompanyName }}</p>
-                                <p class="">
-                                    <x-template.icon>map-marker-outline</x-template.icon>
-                                    {{ $jobInfo->co_address }}
-                                </p>
-                                <div class="mb-4">
-                                    {!! $jobInfo->job_desc !!}
-                                </div>
+                    <p class="display-5 font-weight-bold mb-0">{{ $selectedCompanyName }}</p>
+                    <p class="">
+                        <x-template.icon>map-marker-outline</x-template.icon>
+                        {{ $jobInfo->co_address }}
+                    </p>
+                    <div class="mb-4">
+                        {!! $jobInfo->job_desc !!}
+                    </div>
+                    </div>
 
+                    <div class="form-group mb-0">
+                        <p class="font-weight-bold">Recommended Programs</p>
+                    </div>
 
-                                <p class="font-weight-bold">Recommended Programs</p>
-
-
-
-                                <div>
-                                    <p class="font-weight-bold">Provided Files</p>
-                                    <div class="d-block">
-                                        <button class="btn btn-sm btn-info rounded">Requirement.pdf<x-template.icon>download</x-template.icon></button>
-                                        <button class="btn btn-sm btn-info rounded">Requirement.pdf<x-template.icon>download</x-template.icon></button>
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="form-group mb-0">
+                        <p class="font-weight-bold">Provided Files</p>
+                        <div class="d-block">
+                            <button class="btn btn-sm btn-info rounded">Requirement.pdf<x-template.icon>download</x-template.icon></button>
+                            <button class="btn btn-sm btn-info rounded">Requirement.pdf<x-template.icon>download</x-template.icon></button>
                         </div>
                     </div>
                 </div>
 
-                <!-- Right Side -->
-                <div class="col-xl-6 p-4 border">
-                    <h2>Resumé</h2>
-                    <div class="form-check mx-4 my-0">
-                        <input class="form-check-input" type="radio" wire:model="resumeSelect"
-                            value="useResume" id="useResume">
-                        <label class="form-check-label" for="useResume">
-                            Use resumé from requirements
-                        </label>
-                    </div>
-                    <div class="form-check mx-4 my-0">
-                        <input class="form-check-input" type="radio" wire:model="resumeSelect"
-                            value="uploadResume" id="uploadResume">
-                        <label class="form-check-label" for="uploadResume">
-                            Upload new resumé
-                        </label>
-                    </div>
-                    <div class="form-check mx-4 my-0">
-                        <input class="form-check-input" type="radio" wire:model="resumeSelect"
-                            value="noResume" id="noResume">
-                        <label class="form-check-label" for="noResume">
-                            Do not include a resumé
-                        </label>
-                    </div>
 
-                    <h2>Cover Letter</h2>
-                    <div class="form-check mx-4 my-0">
-                        <input class="form-check-input" type="radio" wire:model="coverSelect"
-                            value="uploadCover" id="uploadCover">
-                        <label class="form-check-label" for="uploadCover">
-                            Upload Cover Letter
-                        </label>
-                    </div>
-                    <div class="form-check mx-4 my-0">
-                        <input class="form-check-input" type="radio" wire:model="coverSelect"
-                            value="writeCover" id="writeCover">
-                        <label class="form-check-label" for="writeCover">
-                            Write a Cover
-                        </label>
-                    </div>
-                    <div class="form-check mx-4 my-0">
-                        <input class="form-check-input" type="radio" wire:model="coverSelect"
-                            value="noCover" id="noCover">
-                        <label class="form-check-label" for="noCover">
-                            Do not include a cover letter
-                        </label>
-                    </div>
-                </div>
 
+                <!--Right Side-->
+                <div class="col-lg-6 d-flex flex-column justify-content-between">
+                    <div>
+                        <h2>Resumé</h2>
+                        <div class="form-check mx-4 my-0">
+                            <input class="form-check-input" type="radio" wire:model="resumeSelect"
+                                value="useResume" id="useResume">
+                            <label class="form-check-label" for="useResume">
+                                Use resumé from requirements
+                            </label>
+                        </div>
+                        <div class="form-check mx-4 my-0">
+                            <input class="form-check-input" type="radio" wire:model="resumeSelect"
+                                value="uploadResume" id="uploadResume">
+                            <label class="form-check-label" for="uploadResume">
+                                Upload new resumé
+                            </label>
+                        </div>
+                        <div class="form-check mx-4 my-0">
+                            <input class="form-check-input" type="radio" wire:model="resumeSelect"
+                                value="noResume" id="noResume">
+                            <label class="form-check-label" for="noResume">
+                                Do not include a resumé
+                            </label>
+                        </div>
+
+                        <h2>Cover Letter</h2>
+                        <div class="form-check mx-4 my-0">
+                            <input class="form-check-input" type="radio" wire:model="coverSelect"
+                                value="uploadCover" id="uploadCover">
+                            <label class="form-check-label" for="uploadCover">
+                                Upload Cover Letter
+                            </label>
+                        </div>
+                        <div class="form-check mx-4 my-0">
+                            <input class="form-check-input" type="radio" wire:model="coverSelect"
+                                value="writeCover" id="writeCover">
+                            <label class="form-check-label" for="writeCover">
+                                Write a Cover
+                            </label>
+                        </div>
+                        <div class="form-check mx-4 my-0">
+                            <input class="form-check-input" type="radio" wire:model="coverSelect"
+                                value="noCover" id="noCover">
+                            <label class="form-check-label" for="noCover">
+                                Do not include a cover letter
+                            </label>
+                        </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 
     @elseif (!$this->company)
     <h1>Manage Job Listings</h1>
