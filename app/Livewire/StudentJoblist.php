6688dd95017e9f706b203b82517182c3faa174f7
OLLC137 <jaunaromin@gmail.com>
2024-11-27 23:33:20 +0800

    Added uploading of letter and resume

--- app/Livewire/StudentJoblist.php
+++ app/Livewire/StudentJoblist.php
@@ -2,18 +2,88 @@
 
 namespace App\Livewire;
 
+use App\Models\OjtRequirement;
+use App\Models\OjtStudent;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 
+use Livewire\WithFileUploads;
+
+
 class StudentJoblist extends Component
 {
+    use WithFileUploads;
+
     #[Url()] public $id;
     public $jobInfo;
     public $jobPrograms;
 
-    public $resumeSelect = 'useResume';
-    public $coverSelect = 'uploadCover';
+    public $resumeSelect;
+    public $selectedResumeFileName;
+
+    public $resumeFile;
+    public $temporaryUploadedResume;
+    public $originalResumeName;
+
+    public $coverSelect;
+
+    public $coverFile;
+    public $temporaryUploadedCover;
+    public $originalCoverName;
+
+    public $writeCover;
+
+    public function updatedResumeSelect()
+    {
+        if ($this->resumeSelect == 'useResume') {
+            $student = OjtStudent::where('user_id', Auth::id())->first(); // Get the student associated with the user
+
+            $requirement = OjtRequirement::where('student_id', $student->id)
+                ->where('req_id', 3)
+                ->where('locked_at', '!=', null)
+                ->first(); // Get the requirement
+            if ($requirement){
+                $this->selectedResumeFileName = $requirement->req_orig_name;
+
+            } else {
+                $this->selectedResumeFileName = 'No Uploaded Resume';
+            }
+        }
+    }
+
+    public function updatedResumeFile()
+    {
+        if ($this->resumeFile) {
+            // Store the uploaded file temporarily
+            $this->temporaryUploadedResume = $this->resumeFile->store('temp');
+            // Store the original file name
+            $this->originalResumeName = $this->resumeFile->getClientOriginalName();
+        }
+    }
+
+    public function updatedCoverFile()
+    {
+        if ($this->coverFile) {
+            // Store the uploaded file temporarily
+            $this->temporaryUploadedCover = $this->coverFile->store('temp');
+            // Store the original file name
+            $this->originalCoverName = $this->coverFile->getClientOriginalName();
+        }
+    }
+    public function clearResumeFile()
+    {
+        // Clear the temporary file
+        $this->resumeFile = null;
+        $this->temporaryUploadedResume = null;
+    }
+    public function clearCoverFile()
+    {
+        // Clear the temporary file
+        $this->coverFile = null;
+        $this->temporaryUploadedCover = null;
+    }
 
     public function render()
     {
--- resources/views/livewire/student-joblist.blade.php
+++ resources/views/livewire/student-joblist.blade.php
@@ -24,15 +24,15 @@
                                 {!! $jobInfo->job_desc !!}
                             </div>
                             @if (!empty($jobPrograms) && count(array_filter($jobPrograms)) > 0)
-                            <p class="font-weight-bold">Recommended Programs</p>
-                            <div class="d-flex flex-row text-white flex-wrap">
-                                @foreach ($jobPrograms as $program)
-                                    <p class="bg-secondary p-1 mx-1 rounded">
-                                        <x-template.icon>tag-outline</x-template.icon>
-                                        {{ $program }}
-                                    </p>
-                                @endforeach
-                            </div>
+                                <p class="font-weight-bold">Recommended Programs</p>
+                                <div class="d-flex flex-row text-white flex-wrap">
+                                    @foreach ($jobPrograms as $program)
+                                        <p class="bg-secondary p-1 mx-1 rounded">
+                                            <x-template.icon>tag-outline</x-template.icon>
+                                            {{ $program }}
+                                        </p>
+                                    @endforeach
+                                </div>
                             @endif
                             <div>
                                 <p class="font-weight-bold">Provided Files</p>
@@ -44,7 +44,106 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="col-xl-6 p-4 border">
+                            <div class="mb-4">
+                                <h2>Resumé</h2>
+                                <div class="my-0" wire:click="$set('resumeSelect', 'useResume')">
+                                    <label>
+                                        <input class="form-check-input" type="radio" wire:model="resumeSelect"
+                                            value="useResume">
+                                        Use resumé from requirements
+                                    </label>
+                                </div>
+                                @if ($resumeSelect == 'useResume')
+                                    <div class="d-flex mt-2 px-4">
+                                        <p class="font-weight-bold">
+                                            {{$selectedResumeFileName}}
+                                            @if ($selectedResumeFileName != 'No Uploaded Resume')
+                                            <x-template.icon>file</x-template.icon>
+                                            @endif
+                                            </p>
+                                    </div>
+                                @endif
+                                <div class="my-0" wire:click="$set('resumeSelect', 'uploadResume')">
+                                    <label>
+                                        <input class="form-check-input" type="radio" wire:model="resumeSelect"
+                                            value="uploadResume">
+                                        Upload new resumé
+                                    </label>
+                                </div>
+                                @if ($resumeSelect == 'uploadResume')
+                                    <div class="d-inline-flex flex-column  mt-2 px-4">
+                                        @if ($temporaryUploadedResume)
+                                            <span class="font-weight-bold">{{ $originalResumeName }}<x-template.icon>file</x-template.icon></span>
+                                            <span class="mb-4 mt-2" role="button" wire:click='clearResumeFile '><u>Upload New File</u></span>
+                                        @else
+                                            <x-template.input type="file" text="" wire:model="resumeFile"
+                                                accept=".pdf,.doc,.docx,.jpg,.jpeg,.png"
+                                                style=""></x-template.input>
+                                        @endif
+                                        @error('resumeFile')
+                                            <div class="mt-2 alert alert-danger">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                @endif
+                                <div class="my-0" wire:click="$set('resumeSelect', 'noResume')">
+                                    <label>
+                                        <input class="form-check-input" type="radio" wire:model="resumeSelect"
+                                            value="noResume">
+                                        Do not include a resumé
+                                    </label>
+                                </div>
+                            </div>
 
+                            <div class="mb-4">
+                                <h2>Cover Letter</h2>
+                                <div class="my-0" wire:click="$set('coverSelect', 'uploadCover')">
+                                    <label>
+                                        <input class="form-check-input" type="radio" wire:model="coverSelect"
+                                            value="uploadCover">
+                                        Upload Cover Letter
+                                    </label>
+                                </div>
+                                @if ($coverSelect == 'uploadCover')
+                                    <div class="d-inline-flex flex-column  mt-2 px-4">
+                                        @if ($temporaryUploadedCover)
+                                            <span class="font-weight-bold">{{ $originalCoverName }}<x-template.icon>file</x-template.icon></span>
+                                            <span class="mb-4 mt-2" role="button" wire:click='clearCoverFile '><u>Upload New File</u></span>
+                                        @else
+                                            <x-template.input type="file" text="" wire:model="coverFile"
+                                                accept=".pdf,.doc,.docx,.jpg,.jpeg,.png"
+                                                style=""></x-template.input>
+                                        @endif
+                                        @error('coverFile')
+                                            <div class="mt-2 alert alert-danger">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                @endif
+                                <div class="my-0" wire:click="$set('coverSelect', 'writeCover')">
+                                    <label>
+                                        <input class="form-check-input" type="radio" wire:model="coverSelect"
+                                            value="writeCover">
+                                        Write a Cover Letter
+                                    </label>
+                                </div>
+                                @if ($coverSelect == 'writeCover')
+                                    <div class="d-flex mt-2 px-4">
+                                        <textarea class="form-control" wire:model="writeCover" rows="10"></textarea>
+                                    </div>
+                                @endif
+                                <div class="my-0" wire:click="$set('coverSelect', 'noCover')">
+                                    <label>
+                                        <input class="form-check-input" type="radio" wire:model="coverSelect"
+                                            value="noCover">
+                                        Do not include a Cover Letter
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
