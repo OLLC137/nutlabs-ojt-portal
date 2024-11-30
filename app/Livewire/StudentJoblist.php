4ca215da5c24acd1d0ccaba13e618ad1db426e3b
OLLC137 <jaunaromin@gmail.com>
2024-12-01 00:14:02 +0800

    Student Panel Joblist

--- app/Livewire/StudentJoblist.php
+++ app/Livewire/StudentJoblist.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\OjtApplicant;
 use App\Models\OjtRequirement;
 use App\Models\OjtDownloadable;
+use App\Models\OjtJobListing;
 use App\Models\OjtStudent;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -13,9 +14,67 @@ use Livewire\Attributes\Url;
 use Livewire\Component;
 
 use Livewire\WithFileUploads;
-
+use Livewire\WithPagination;
 
 class StudentJoblist extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
 
+    public $search;
+    public $category;
+    public $location;
+    public $program;
+    public $categories;
+
+    public function selectJob($id)
+    {
+        return redirect('/student-joblist/' . $id);
+    }
+    public function doSearch(){
+
+    }
+    public function render()
+    {
+        $query = OjtJobListing::query()
+            ->join('ojt_companies', 'ojt_companies.id', '=', 'ojt_job_listings.company_id')
+            ->join('ojt_job_list_categories', 'ojt_job_listings.job_category', '=', 'ojt_job_list_categories.id')
+            ->select(
+                'ojt_job_listings.id',
+                'job_ref',
+                'co_name',
+                'job_list',
+                'ojt_job_list_categories.cat_name as job_category',
+                'job_status'
+            );
+
+        // Apply search filters if they exist
+        if ($this->search) {
+            $searchTerm = '%' . strtolower($this->search) . '%';
+            $query->where(function ($query) use ($searchTerm) {
+                $query->whereRaw('LOWER(ojt_job_listings.job_list) LIKE ?', [$searchTerm])
+                    ->orWhereRaw('LOWER(ojt_companies.co_name) LIKE ?', [$searchTerm])
+                    ->orWhereRaw('LOWER(ojt_job_listings.job_ref) LIKE ?', [$searchTerm]);
+            });
+        }
+        if ($this->location) {
+            $query->whereRaw('LOWER(ojt_companies.co_address) LIKE ?', ['%' . strtolower($this->location) . '%']);
+        }
+        if ($this->category) {
+            $query->whereRaw('LOWER(ojt_job_list_categories.cat_name) = ?', [strtolower($this->category)]);
+        }
+        if ($this->program) {
+            $programTerm = '%' . strtolower($this->program) . '%';
+            $query->where(function ($query) use ($programTerm) {
+                $query->whereRaw('LOWER(ojt_job_listings.job_programs) LIKE ?', [$programTerm]);
+            });
+        }
+
+        $this->categories = DB::table('ojt_job_list_categories')
+            ->orderBy('cat_name')
+            ->pluck('cat_name', 'id');
+        return view('livewire.student-joblist', [
+            'jobListings' => $query->paginate(10),
+        ]);
+    }
 }
--- resources/views/livewire/student-joblist.blade.php
+++ resources/views/livewire/student-joblist.blade.php
@@ -1,3 +1,67 @@
 <div>
-    <h1>Job Listings</h1>
+    <div class="row align-items-end px-3">
+        <div class="col-lg-2 px-1 my-lg-0 my-2">
+            Search
+            <div class="input-group">
+                <input type="text" class="form-control" wire:model="search" wire:keydown.enter="doSearch"
+                    placeholder="Enter Keywords">
+            </div>
+        </div>
+        <div class="col-lg-2 px-1 my-lg-0 my-2">
+            Categories
+            <select style="height: 45px;" class="form-select" wire:model="category" placeholder="Any Classifications">
+                <option value="">Any Classification</option>
+                @foreach ($categories as $id => $cat_name)
+                    <option value="{{ $cat_name }}">{{ $cat_name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <div class="col-lg-2 px-1 my-lg-0 my-2">
+            Program
+            <input type="text" class="form-control" wire:model="program" wire:keydown.enter="doSearch"
+                placeholder="Enter Program Name">
+        </div>
+        <div class="col-lg-2 px-1 my-lg-0 my-2">
+            Location
+            <input type="text" class="form-control" wire:model="location" wire:keydown.enter="doSearch"
+                placeholder="Enter Suburb, City, or Reigon">
+        </div>
+        <div class="col-lg-2 px-1 my-lg-0 my-2">
+            <x-template.button color="primary" wire:click="doSearch">
+                <i class="mdi mdi-magnify"></i>
+            </x-template.button>
+            <x-template.icon wire:click="$set('search', ''); $set('program', ''); $set('location', '')"
+                class="text-danger" role="button">close</x-template.icon>
+        </div>
+
+    </div>
+    <div class="card my-2">
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Job List</th>
+                        <th>Job Category</th>
+                        <th>Company Name</th>
+                        <th>Job Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($jobListings as $job)
+                        <tr role="button" wire:click="selectJob({{ $job->id }})">
+                            <td>{{ $job->job_list }}</td>
+                            <td>{{ $job->job_category }}</td>
+                            <td>{{ $job->co_name }}</td>
+                            @if ($job->job_status)
+                                <td><label class="badge badge-success">OPEN</label></td>
+                            @else
+                                <td><label class="badge badge-primary">CLOSED</label></td>
+                            @endif
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    {{ $jobListings->links() }}
 </div>
