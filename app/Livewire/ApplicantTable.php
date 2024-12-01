fe56787e8bdc44714d097be99a8b01f0144c71b1
OLLC137 <jaunaromin@gmail.com>
2024-12-01 14:37:40 +0800

    Application Acceptance and Rejection

--- app/Livewire/ApplicantTable.php
+++ app/Livewire/ApplicantTable.php
@@ -6,79 +6,110 @@ use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\OjtApplicant;
 use App\Models\OjtCompany;
+use App\Models\OjtDownloadable;
 use App\Models\OjtJobListing;
+use App\Models\OjtRequirement;
 use Illuminate\Support\Facades\Auth;
 
 class ApplicantTable extends Component
 {
     use WithPagination;
-
     protected $paginationTheme = 'bootstrap';
 
     public $searchQuery = '';
-    public $applicantIdToDelete;
-    public $actionType;
-    public $successMessage;
+    public $applicantId;
+    public $selectedResumeFile;
+    public $selectedCoverFile;
+
+    public function mount() // Default to null
+    {
+        $this->applicantId = $applicantId ?? session()->pull('applicantId'); // Use session if not provided
+    }
 
     public function search()
     {
         $this->resetPage();
     }
 
-    public function confirmAction($applicantId, $type)
-    {
-        $this->applicantIdToDelete = $applicantId;
-        $this->actionType = $type;
-    }
-
-    public function executeAction()
-    {
-        $applicant = OjtApplicant::find($this->applicantIdToDelete);
-
-        if ($applicant) {
-            if ($this->actionType === 'accept') {
-                $applicant->update(['status' => 2]);
-                $this->successMessage = 'Applicant successfully accepted.';
-            } elseif ($this->actionType === 'delete') {
-                $applicant->delete();
-                $this->successMessage = 'Applicant successfully deleted.';
-            }
-
-            $this->resetPage();
-        }
-
-        $this->applicantIdToDelete = null;
-        $this->actionType = null;
-    }
-
     public function render()
     {
-        $user = Auth::user();
+        if ($this->applicantId) {
+            $applicant = OjtApplicant::join('ojt_students', 'ojt_students.id', '=', 'ojt_applicants.student_id')
+                ->join('ojt_job_listings', 'ojt_job_listings.id', '=', 'ojt_applicants.joblist_id')
+                ->where('ojt_applicants.id', $this->applicantId)->first();
 
-        $companyId = OjtCompany::where('user_id', $user->id)->first()->id;
+            if ($applicant->resume_mode == 1) {
+                $this->selectedResumeFile = OjtRequirement::where('id', $applicant->resume_file_id)->first();
+            } else if ($applicant->resume_mode == 2) {
+                $this->selectedResumeFile = OjtDownloadable::where('id', $applicant->resume_file_id)->first();
+            } else $this->selectedResumeFile = null;
+
+            if ($applicant->cover_mode == 1) {
+                $this->selectedCoverFile = OjtDownloadable::where('id', $applicant->cover_file_id)->first();
+            } else $this->selectedCoverFile = null;
+            return view('livewire.applicant-table', [
+                'applicant' => $applicant,
+            ]);
+        } else {
+            $user = Auth::user();
+
+            $companyId = OjtCompany::where('user_id', $user->id)->first()->id;
             $jobListIds = OjtJobListing::where('company_id', $companyId)->pluck('id')->toArray();
             $query = OjtApplicant::query();
 
-        // Apply search query if it exists
-        if (!empty($this->searchQuery)) {
-            $searchQuery = strtolower($this->searchQuery);
-            $query->whereHas('student', function ($q) use ($searchQuery) {
-                $q->whereRaw('LOWER(stud_first_name) LIKE ?', ['%' . $searchQuery . '%'])
-                  ->orWhereRaw('LOWER(stud_last_name) LIKE ?', ['%' . $searchQuery . '%'])
-                  ->orWhereRaw('LOWER(stud_sr_code) LIKE ?', ['%' . $searchQuery . '%']);
-            });
+            // Apply search query if it exists
+            if (!empty($this->searchQuery)) {
+                $searchQuery = strtolower($this->searchQuery);
+                $query->whereHas('student', function ($q) use ($searchQuery) {
+                    $q->whereRaw('LOWER(stud_first_name) LIKE ?', ['%' . $searchQuery . '%'])
+                        ->orWhereRaw('LOWER(stud_last_name) LIKE ?', ['%' . $searchQuery . '%'])
+                        ->orWhereRaw('LOWER(stud_sr_code) LIKE ?', ['%' . $searchQuery . '%']);
+                });
+            }
+            // Exclude applicants with a status of 1 (accepted)
+            $applicants = $query->whereIn('joblist_id', $jobListIds)
+                ->where('status', '!=', 3)
+                ->orderBy('status', 'desc')
+                ->orderBy('created_at', 'desc')
+                ->paginate(10, ['*'], 'applicants-page');
+
+            return view('livewire.applicant-table', [
+                'applicants' => $applicants,
+            ]);
         }
-
-        // Exclude applicants with a status of 1 (accepted)
-        $applicants = $query->whereIn('joblist_id', $jobListIds)->paginate(10, ['*'], 'applicants-page');
-
-        return view('livewire.applicant-table', [
-            'applicants' => $applicants,
-        ]);
     }
+    public function downloadFile($id)
+    {
+        $file = OjtDownloadable::where('id', $id)
+            ->first();
 
+        $filePath = storage_path('app/' . $file->file_path);
+        return response()->download($filePath, $file->file_original_name);
+    }
+    public function downloadRequirement($id)
+    {
+        $file = OjtRequirement::where('id', $id)
+            ->first();
+
+        $filePath = storage_path('app/' . $file->req_file_path);
+        return response()->download($filePath, $file->req_orig_name);
+    }
     public function clearSearch()
     {
         $this->searchQuery = '';
+    }
+    public function selectApplicant($id)
+    {
+        $this->applicantId = $id;
+    }
+    public function accept()
+    {
+        session()->flash('status', 'Successfully accepted applicant.');
+        OjtApplicant::where('id', $this->applicantId)->update(['status' => 1]);
+    }
+    public function reject()
+    {
+        session()->flash('status', 'Successfully rejected applicant.');
+        OjtApplicant::where('id', $this->applicantId)->update(['status' => 3]);
     }
 }
--- resources/views/pages/view-applicants.blade.php
+++ resources/views/pages/view-applicants.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
     <x-slot name="header"></x-slot>
-    <h1>Applicant List</h1>
     @livewire('applicant-table')
 </x-app-layout>
