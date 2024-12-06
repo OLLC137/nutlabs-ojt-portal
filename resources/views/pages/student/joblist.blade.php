56e5fcb84addb7b26089ebf35cd2b5a4632c0d9c
OLLC137 <jaunaromin@gmail.com>
2024-12-06 17:37:34 +0800

    Removed string

--- resources/views/pages/student/joblist.blade.php
+++ resources/views/pages/student/joblist.blade.php
@@ -2,7 +2,6 @@
     <x-slot name="header"></x-slot>
     <div class="d-flex justify-content-between">
         <h1>Job Listings</h1>
-        <a href="/joblist">you can also view job postings in the home page</a>
     </div>
     @livewire('student-joblist')
 </x-app-layout>
